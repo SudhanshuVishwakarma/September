2f59f902cdb5bba7f11d0cd08c17c97c09bfa930
SudhanshuVishwakarma <vsudhanshu18@gmail.com>
2023-09-24 11:50:36 +0530

    hill

--- star.py
+++ star.py
@@ -1,5 +1,42 @@
-for i in range(1, 5):
-    for j in range(1, 5):
-        if j >= i:
-            print("*", end=" ")
+# for i in range(5):
+#     for j in range(i + 1):
+#         print("*", end=" ")
+#     print()
+n = 5
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(" ", end=" ")
+#     for j in range(i, n):
+#         print("*", end=" ")
+
+#     print()
+
+# for i in range(n - 1):
+#     for j in range(i, n):
+#         print(" ", end=" ")
+#     for j in range(i):
+#         print("*", end=" ")
+#     for j in range(i + 1):
+#         print("*", end=" ")
+#     print()
+
+# for i in range(n):
+#     for j in range(i + 1):
+#         print(" ", end=" ")
+#     for j in range(i, n - 1):
+#         print("*", end=" ")
+#     for j in range(i, n):
+#         print("*", end=" ")
+#     print()
+
+
+for i in range(n):
+    for j in range(i, n):
+        print(" ", end=" ")
+    for j in range(i + 1):
+        print("*", end=" ")
+    for j in range(i):
+        print("*", end=" ")
+    for j in range(i, n):
+        print(" ", end=" ")
     print()
